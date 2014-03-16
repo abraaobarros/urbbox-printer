03f3d0d55d0c792d1ae5194cd76e1a9f202b574a
Abraao Barros Laceda <abraaobarros3@gmail.com>
2014-03-16 03:17:03 -0300

    led

--- urbbox.py
+++ urbbox.py
@@ -2,6 +2,7 @@ from Adafruit_Thermal import *
 import json
 
 import requests
+import subprocess, time, Image, socket
 import RPi.GPIO as GPIO
 
 ledPin       = 18
@@ -17,8 +18,12 @@ GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.output(ledPin, GPIO.HIGH)
 time.sleep(30)
 
+
+
+
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 GPIO.output(ledPin, GPIO.HIGH)
+
 
 #login
 login_data =  {'username':'demo', 'password':'123456'}
@@ -27,19 +32,52 @@ s.post('http://www.urbbox.com.br/admin',login_data)
 
 r = s.get('http://www.urbbox.com.br/backend?format=json')
 
+def print_list_orders():
+	pedidos = json.loads(r.text)["pedidos"]
+	for p in pedidos:
+		printer.justify('C')
+		printer.println("**********************")
+		printer.feed(1)
+		printer.setSize('L')
+		printer.boldOn()
+		printer.println("Mesa "+p["rua"])
+		printer.boldOff()
+		printer.println(str(p["listaItens"][0]["numero"]) + "   " + p["listaItens"][0]["nome"].encode('utf8') + "  -  "+ p["listaItens"][0]["observacao"].encode('utf8') +"  "+ str(p["listaItens"][0]["quantidade"]))
+		printer.feed(2)
+	printer.feed(4)
+	GPIO.output(ledPin, GPIO.LOW)
 
-pedidos = json.loads(r.text)["pedidos"]
-for p in pedidos:
-	printer.justify('C')
-	printer.println("**********************")
-	printer.feed(1)
-	printer.setSize('L')
-	printer.boldOn()
-	printer.println("Mesa "+p["rua"])
-	printer.boldOff()
-	printer.println(str(p["listaItens"][0]["numero"]) + "   " + p["listaItens"][0]["nome"].encode('utf8') + "  -  "+ p["listaItens"][0]["observacao"].encode('utf8') +"  "+ str(p["listaItens"][0]["quantidade"]))
-	printer.feed(2)
-printer.feed(4)
-GPIO.output(ledPin, GPIO.LOW)
+def tap():
+	printer.println("Apertei")
+def hold():
+	printer.println("Segurei")
+	print_list_orders()
+
+while(True):
+	# Poll current button state and time
+  buttonState = GPIO.input(buttonPin)
+  t           = time.time()
+
+  # Has button state changed?
+  if buttonState != prevButtonState:
+    prevButtonState = buttonState   # Yes, save new state/time
+    prevTime        = t
+  else:                             # Button state unchanged
+    if (t - prevTime) >= holdTime:  # Button held more than 'holdTime'?
+      # Yes it has.  Is the hold action as-yet untriggered?
+      if holdEnable == True:        # Yep!
+        hold()                      # Perform hold action (usu. shutdown)
+        holdEnable = False          # 1 shot...don't repeat hold action
+        tapEnable  = False          # Don't do tap action on release
+    elif (t - prevTime) >= tapTime: # Not holdTime.  tapTime elapsed?
+      # Yes.  Debounced press or release...
+      if buttonState == True:       # Button released?
+        if tapEnable == True:       # Ignore if prior hold()
+          tap()                     # Tap triggered (button released)
+          tapEnable  = False        # Disable tap and hold
+          holdEnable = False
+      else:                         # Button pressed
+        tapEnable  = True           # Enable tap and hold actions
+        holdEnable = True
 
 
