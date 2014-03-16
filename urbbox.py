a05ed8d0fa0691632f553d9f500a89ffc025c732
Abraao Barros Laceda <abraaobarros3@gmail.com>
2014-03-16 10:29:48 -0300

    a

--- urbbox.py
+++ urbbox.py
@@ -61,13 +61,16 @@ n_pedido = 0
 qtd = 0
 def check_novos_pedidos():
 	print "checkando pedidos"
-	r2 = s.get("http://2.preguicosotest.appspot.com/pedidos/1323001")
-	print r2.text
-	qtd = json.loads(unicode(r2.text))['qtd']
-	if n_pedido != qtd:
-		print_list_orders()
-		n_pedido=qtd
-
+	try:
+		r2 = s.get("http://2.preguicosotest.appspot.com/pedidos/1323001")
+		print r2.json
+		qtd = json.loads(r2.text)['qtd']
+		if n_pedido != qtd:
+			print_list_orders()
+			n_pedido=qtd
+	except Exception, e:
+		printer.println(str(e))
+	
 
 def tap():
 	printer.println("Apertei")
