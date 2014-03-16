143664da0e66ff8f0eeb6c7def782c54c2448f8b
Abraao Barros Laceda <abraaobarros3@gmail.com>
2014-03-16 03:38:43 -0300

    a

--- urbbox.py
+++ urbbox.py
@@ -18,6 +18,7 @@ GPIO.setmode(GPIO.BCM)
 # Enable LED and button (w/pull-up on latter)
 GPIO.setup(ledPin, GPIO.OUT)
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setwarnings(False)
 
 # LED on while working
 GPIO.output(ledPin, GPIO.HIGH)
@@ -27,36 +28,49 @@ time.sleep(30)
 
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
-GPIO.output(ledPin, GPIO.HIGH)
+
 
 
 #login
 login_data =  {'username':'demo', 'password':'123456'}
+id_est = 1323001
 s = requests.session()
 s.post('http://www.urbbox.com.br/admin',login_data)
 
 r = s.get('http://www.urbbox.com.br/backend?format=json')
+pedidos = json.loads(r.text)["pedidos"]
 
 def print_list_orders():
-	pedidos = json.loads(r.text)["pedidos"]
+	GPIO.output(ledPin, GPIO.HIGH)
 	for p in pedidos:
 		printer.justify('C')
 		printer.println("**********************")
 		printer.feed(1)
 		printer.setSize('L')
 		printer.boldOn()
+		printer.doubleHeightOn()
 		printer.println("Mesa "+p["rua"])
 		printer.boldOff()
 		printer.println(str(p["listaItens"][0]["numero"]) + "   " + p["listaItens"][0]["nome"].encode('utf8') + "  -  "+ p["listaItens"][0]["observacao"].encode('utf8') +"  "+ str(p["listaItens"][0]["quantidade"]))
 		printer.feed(2)
+		printer.doubleHeightOFF()
 	printer.feed(4)
 	GPIO.output(ledPin, GPIO.LOW)
+
+n_pedido = 0
+def check_novos_pedidos():
+	r2 = s.get("http://2.preguicosotest.appspot.com/pedidos/1323001")
+	qtd = json.loads(r2.text)['qtd']
+	if n_pedido != qtd:
+		print_list_orders()
+		n_pedido=qtd
+
 
 def tap():
 	printer.println("Apertei")
 def hold():
 	printer.println("Segurei")
-	print_list_orders()
+
 
 # Poll initial button state and time
 prevButtonState = GPIO.input(buttonPin)
@@ -67,7 +81,7 @@ holdEnable      = False
 while(True):
 	# Poll current button state and time
   buttonState = GPIO.input(buttonPin)
-  t           = time.time()
+  t= time.time()
 
   # Has button state changed?
   if buttonState != prevButtonState:
@@ -90,5 +104,10 @@ while(True):
       else:                         # Button pressed
         tapEnable  = True           # Enable tap and hold actions
         holdEnable = True
-
+ 
+  if ((int(t) & 1) == 0) and ((t - int(t)) < 0.15):
+    GPIO.output(ledPin, GPIO.HIGH)
+    check_novos_pedidos()
+  else:
+    GPIO.output(ledPin, GPIO.LOW)
 
