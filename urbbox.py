acbf94cc0394310348d9b6ef91d5b1f17f1b78ff
Abraao Barros Laceda <abraaobarros3@gmail.com>
2014-03-16 02:54:52 -0300

    led

--- urbbox.py
+++ urbbox.py
@@ -4,21 +4,21 @@ import json
 import requests
 # import RPi.GPIO as GPIO
 
-# ledPin       = 18
-# buttonPin    = 23
+ledPin       = 18
+buttonPin    = 23
 
-# GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-# # Enable LED and button (w/pull-up on latter)
-# GPIO.setup(ledPin, GPIO.OUT)
-# GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Enable LED and button (w/pull-up on latter)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# # LED on while working
-# GPIO.output(ledPin, GPIO.HIGH)
-# time.sleep(30)
+# LED on while working
+GPIO.output(ledPin, GPIO.HIGH)
+time.sleep(30)
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
-# GPIO.output(ledPin, GPIO.HIGH)
+GPIO.output(ledPin, GPIO.HIGH)
 
 #login
 login_data =  {'username':'demo', 'password':'123456'}
@@ -40,6 +40,6 @@ for p in pedidos:
 	printer.println(str(p["listaItens"][0]["numero"]) + "   " + p["listaItens"][0]["nome"].encode('utf8') + "  -  "+ p["listaItens"][0]["observacao"].encode('utf8') +"  "+ str(p["listaItens"][0]["quantidade"]))
 	printer.feed(2)
 printer.feed(4)
-# GPIO.output(ledPin, GPIO.LOW)
+GPIO.output(ledPin, GPIO.LOW)
 
 
